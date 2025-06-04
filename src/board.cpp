091ac99c681e1b804309ebcbda950231fd7a592f
Nicholas Gledhill <nick@creativearts.com.au>
2025-06-04 13:19:03 +0000

    message to Wolfie

--- src/board.cpp
+++ src/board.cpp
@@ -4,5 +4,7 @@
 Board::Board() {}
 
 void Board::print() {
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hi Wolfgang." << std::endl;
     std::cout << "Board goes here!" << std::endl;
 }
