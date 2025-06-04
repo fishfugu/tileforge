a9d88c5207012aad20fc3d2ed6df027d5df1fa78
Nicholas Gledhill <nick@creativearts.com.au>
2025-06-04 13:15:12 +0000

    initial set-up

---
+++ build/CMakeFiles/tileforge.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tileforge.dir/src/board.cpp.o"
+  "CMakeFiles/tileforge.dir/src/main.cpp.o"
+  "tileforge"
+  "tileforge.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tileforge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
